c41745200750eea59a54443440c058f4745425da
wasfijr <115897361+wasfijr@users.noreply.github.com>
2026-05-09 16:26:10 +0300

    fixed the CustomUserDetailsService.java class

--- backend/src/main/java/com/example/hybridflow/security/CustomUserDetailsService.java
+++ backend/src/main/java/com/example/hybridflow/security/CustomUserDetailsService.java
@@ -21,12 +21,8 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String email)
             throws UsernameNotFoundException {
 
-        User user = userRepository.findByEmail(email)
-                .orElseThrow(() ->
-                        new UsernameNotFoundException(
-                                "User not found with email: " + email
-                        )
-                );
+        User user = userRepository.findAuthContextByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
 
         return new CustomUserDetails(user);
     }
